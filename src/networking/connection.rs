fbce998cbc9976d8ecae2d75953c3045cfaf9b1b
Jak Tiano <jak33@mac.com>
2021-12-18 00:53:15 -0500

    refactored connection to new mod, cleaned up names

---
+++ src/networking/connection.rs
@@ -0,0 +1,143 @@
+use core::fmt;
+use futures::{Sink, SinkExt, Stream, StreamExt};
+use std::net::SocketAddr;
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+
+pub(crate) type MessageTx = UnboundedSender<(WireMessage, UnboundedSender<WireMessage>)>;
+pub(crate) type MessageRx = UnboundedReceiver<(WireMessage, UnboundedSender<WireMessage>)>;
+
+// =============== enum connection::Protocol ===============
+///
+/// Defines the implemented networking protocols that are available to use.
+
+#[derive(Clone, Copy)]
+pub(crate) enum Protocol {
+    WebSocket,
+}
+impl fmt::Display for Protocol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let protocol_string = match self {
+            Protocol::WebSocket => "ws://",
+        };
+        write!(f, "{}", protocol_string)
+    }
+}
+
+// =============== enum connection::WireMessage ===============
+///
+/// This is the raw coalescent-computer message that we send across the wire.
+/// It will ultimately be wrapped by the protocol that transmits the message,
+/// but at the CC abstraction layer, this is as low as it gets.
+
+pub(crate) enum WireMessage {
+    Empty,
+    Rpc,
+    Ping(Vec<u8>),
+    Pong(Vec<u8>),
+}
+
+// ========= connection::Error enum ===========
+
+#[derive(Debug)]
+pub enum Error {
+    ProtocolError,
+}
+
+// ========= connection::Connection struct ===========
+
+pub(crate) struct Connection<TMessage, TError, TSink, TStream>
+where
+    TMessage: Into<WireMessage> + From<WireMessage>,
+    TError: Into<Error>,
+    TSink: Sink<TMessage, Error = TError>,
+    TStream: Stream<Item = Result<TMessage, TError>>,
+{
+    pub protocol: Protocol,
+    pub address: SocketAddr,
+    pub sink: TSink,
+    pub stream: TStream,
+    pub message_tx: MessageTx,
+}
+
+/// ============ async fn handle_connection(connection: Connection) ============
+///
+/// Takes ownership of a Connection struct and initiates tokio tasks to read
+/// and process messages from the stream, as well as send messages to the sink.
+///
+pub(crate) async fn handle_connection<TMessage, TError, TSink, TStream>(
+    connection: Connection<TMessage, TError, TSink, TStream>,
+) -> Result<(), Error>
+where
+    TMessage: Into<WireMessage> + From<WireMessage> + Send,
+    TError: Into<Error>,
+    TSink: Sink<TMessage, Error = TError> + Unpin + Send + Sync + 'static,
+    TStream: Stream<Item = Result<TMessage, TError>> + Send + Unpin,
+{
+    // create channel for responses to be sent through
+    let (response_tx, mut response_rx) = mpsc::unbounded_channel::<WireMessage>();
+
+    // begin a task for handling responses
+    let mut sink = connection.sink; // move `sink` into a mutable owner
+    tokio::spawn(async move {
+        while let Some(response) = response_rx.recv().await {
+            let protocol_message = response.into();
+            match sink.send(protocol_message).await {
+                Ok(()) => {}
+                Err(_) => {}
+            };
+            println!(
+                "{}{} response sent",
+                connection.protocol, connection.address
+            );
+        }
+    });
+
+    // read incoming connection messages and send to message handler until the stream closes
+    let mut stream = connection.stream; // move stream into mutable owner
+    while let Some(protocol_message) = stream.next().await {
+        println!("{}{} request recd", connection.protocol, connection.address);
+
+        // convert protocol message into wire message
+        let wire_message = match protocol_message {
+            Ok(protocol_message) => protocol_message.into(),
+            Err(err) => return Err(err.into()),
+        };
+
+        // send request to message handler with response channel
+        if let Err(_send_error) = connection
+            .message_tx
+            .send((wire_message, response_tx.clone()))
+        {
+            println!("cannot process request, request_rx handler channel is closed");
+        }
+    }
+
+    // other side closed the websocket connection
+    println!("{}{} closed", connection.protocol, connection.address);
+
+    Ok(())
+}
+
+pub(crate) async fn handle_messages(mut message_rx: MessageRx) -> Result<(), Error> {
+    while let Some(message) = message_rx.recv().await {
+        let response = match message.0 {
+            WireMessage::Empty => None,
+            WireMessage::Rpc => None,
+            WireMessage::Ping(bytes) => Some(WireMessage::Pong(bytes)),
+            WireMessage::Pong(_) => None,
+        };
+
+        // if response exists, pass back response, handle error
+        if let Some(response) = response {
+            if let Err(err) = message.1.send(response) {
+                println!(
+                    "dropped response, response_rx channel was closed. {}",
+                    err.to_string()
+                );
+            }
+        }
+    }
+
+    // transmit channels all closed gracefully
+    Ok(())
+}
--- src/networking/websocket.rs
+++ src/networking/websocket.rs
@@ -4,79 +4,80 @@ use tokio::{
     net::{TcpListener, TcpSocket},
     sync::mpsc::{self},
 };
-use tokio_tungstenite::{client_async, tungstenite::Message};
+use tokio_tungstenite::{client_async, tungstenite};
 
-use crate::networking::{self, Connection, ConnectionError};
+use crate::networking::connection::{self, Connection, WireMessage};
 
-impl Into<ConnectionError> for tokio_tungstenite::tungstenite::Error {
-    fn into(self) -> ConnectionError {
-        ConnectionError::WebSocketHandshakeFailure
+impl From<tokio_tungstenite::tungstenite::Error> for connection::Error {
+    fn from(_: tokio_tungstenite::tungstenite::Error) -> connection::Error {
+        connection::Error::ProtocolError
     }
 }
 
-impl From<networking::Response> for Message {
-    fn from(response: networking::Response) -> Self {
-        Message::Text(String::from("message"))
+/// Convert a
+impl From<WireMessage> for tungstenite::Message {
+    fn from(_: WireMessage) -> Self {
+        tungstenite::Message::Text(String::from("message"))
     }
 }
-impl From<Message> for networking::Message {
-    fn from(message: Message) -> Self {
+impl From<tungstenite::Message> for WireMessage {
+    fn from(message: tungstenite::Message) -> Self {
         match message {
-            Message::Text(text) => {
+            tungstenite::Message::Text(text) => {
                 println!("Received text: {}", text);
-                networking::Message::Empty
+                WireMessage::Empty
             }
-            Message::Binary(_) => {
+            tungstenite::Message::Binary(_) => {
                 println!("Received some binary");
-                networking::Message::Empty
+                WireMessage::Empty
             }
-            Message::Ping(_) => networking::Message::Empty,
-            Message::Pong(_) => networking::Message::Empty,
-            Message::Close(_) => networking::Message::Empty,
+            tungstenite::Message::Ping(_) => WireMessage::Empty,
+            tungstenite::Message::Pong(_) => WireMessage::Empty,
+            tungstenite::Message::Close(_) => WireMessage::Empty,
         }
     }
 }
 
 pub struct Client;
 impl Client {
-    pub async fn connect_local(port: u16) -> Result<(), ConnectionError> {
+    pub async fn connect_local(port: u16) -> Result<(), connection::Error> {
         let address = SocketAddr::from(([127, 0, 0, 1], port));
         Client::connect(address).await?;
         Ok(())
     }
-    pub async fn connect(address: SocketAddr) -> Result<(), ConnectionError> {
-        let uri = Client::get_uri(address);
+    pub async fn connect(address: SocketAddr) -> Result<(), connection::Error> {
+        let uri = Client::get_ws_uri(address);
         let socket = match TcpSocket::new_v4() {
             Ok(sock) => sock,
-            Err(_) => return Err(ConnectionError::WebSocketHandshakeFailure),
+            Err(_) => return Err(connection::Error::ProtocolError),
         };
         let stream = match socket.connect(address).await {
             Ok(strm) => strm,
-            Err(_) => return Err(ConnectionError::WebSocketHandshakeFailure),
+            Err(_) => return Err(connection::Error::ProtocolError),
         };
         let (websocket, _http_response) = match client_async(uri, stream).await {
             Ok(ws) => ws,
-            Err(_) => return Err(ConnectionError::WebSocketHandshakeFailure),
+            Err(_) => return Err(connection::Error::ProtocolError),
         };
 
-        let (request_tx, request_rx) = mpsc::unbounded_channel();
+        let (message_tx, message_rx) = mpsc::unbounded_channel();
         let (ws_sink, ws_stream) = websocket.split();
         let connection = Connection {
-            protocol: networking::Protocol::WebSocket,
+            protocol: connection::Protocol::WebSocket,
             address: address,
-            stream: ws_stream,
             sink: ws_sink,
-            request_tx: request_tx,
+            stream: ws_stream,
+            message_tx,
         };
-        let connection_task = tokio::spawn(Connection::handle_connection(connection));
-        let receive_task = tokio::spawn(networking::handle_requests(request_rx));
+        let connection_task = tokio::spawn(connection::handle_connection(connection));
+        let receive_task = tokio::spawn(connection::handle_messages(message_rx));
 
         future::select(receive_task, connection_task).await;
 
         Ok(())
     }
 
-    fn get_uri(address: SocketAddr) -> String {
+    fn get_ws_uri(address: SocketAddr) -> String {
         format!(
             "ws://{address}:{port}",
             address = address.ip().to_string(),
@@ -87,60 +88,60 @@ impl Client {
 
 pub struct Server;
 impl Server {
-    pub async fn listen_local(port: u16) -> Result<(), ConnectionError> {
+    pub async fn listen_local(port: u16) -> Result<(), connection::Error> {
         let address = SocketAddr::from(([127, 0, 0, 1], port));
         Server::listen(address).await?;
         Ok(())
     }
-    pub async fn listen(address: SocketAddr) -> Result<(), ConnectionError> {
+    pub async fn listen(address: SocketAddr) -> Result<(), connection::Error> {
         let tcp_listener = match TcpListener::bind(address).await {
             Ok(listener) => listener,
-            Err(_) => return Err(ConnectionError::WebSocketHandshakeFailure),
+            Err(_) => return Err(connection::Error::ProtocolError),
         };
         println!("ws:// listening on: {}", address.to_string());
 
-        // create transmit/receive async channels
-        let (request_tx, request_rx) = mpsc::unbounded_channel();
+        // create channels to pass received messages
+        let (message_tx, message_rx) = mpsc::unbounded_channel();
 
         // create listening task
-        let listen_task = tokio::spawn(Server::listen_for_connections(tcp_listener, request_tx));
+        let listen_task = tokio::spawn(Server::accept_connections(tcp_listener, message_tx));
 
         // create receiving task
-        let receive_task = tokio::spawn(networking::handle_requests(request_rx));
+        let receive_task = tokio::spawn(connection::handle_messages(message_rx));
 
         future::select(listen_task, receive_task).await;
 
         Ok(())
     }
 
-    async fn listen_for_connections(
+    async fn accept_connections(
         tcp_listener: TcpListener,
-        request_tx: networking::RequestTx,
+        message_tx: connection::MessageTx,
     ) -> Result<(), std::io::Error> {
         // accept incoming connections until tcp listener closes
         while let Ok((tcp_stream, address)) = tcp_listener.accept().await {
             println!("Incoming TCP connection from: {}", address);
 
-            let request_tx_clone = request_tx.clone();
+            let message_tx_clone = message_tx.clone();
             tokio::spawn(async move {
                 let websocket = match tokio_tungstenite::accept_async(tcp_stream).await {
                     Ok(ws) => ws,
                     Err(_) => {
-                        println!("ws:// connection failed with: {}", address);
+                        println!("ws://{} connection failed ", address);
                         return;
                     }
                 };
                 println!("ws:// opened with: {}", address);
                 let (ws_sink, ws_stream) = websocket.split();
                 let connection = Connection {
-                    protocol: networking::Protocol::WebSocket,
+                    protocol: connection::Protocol::WebSocket,
                     address: address,
-                    stream: ws_stream,
                     sink: ws_sink,
-                    request_tx: request_tx_clone,
+                    stream: ws_stream,
+                    message_tx: message_tx_clone,
                 };
-                if let Err(_) = Connection::handle_connection(connection).await {
-                    println!("ws:// connection errored with: {}", address);
+                if let Err(err) = connection::handle_connection(connection).await {
+                    println!("ws://{} connection error {{{:?}}}", address, err);
                     return;
                 }
             });
